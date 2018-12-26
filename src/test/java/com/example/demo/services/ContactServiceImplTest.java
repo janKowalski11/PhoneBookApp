8f8352c5ea096cf5b31a45586524f28196b23724
BeGieU <marcinoo890@gmail.com>
2018-12-26 12:25:08 +0100

    created added Contact Service Impl

--- src/test/java/com/example/demo/services/ContactServiceImplTest.java
+++ src/test/java/com/example/demo/services/ContactServiceImplTest.java
@@ -1,22 +1,48 @@
 package com.example.demo.services;
 
+import com.example.demo.Model.Contact;
+import com.example.demo.Repositories.ContactRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ContactServiceImplTest
 {
 
+    ContactServiceImpl contactService;
+
+    @Mock
+    ContactRepository contactRepository;
 
 
     @Before
     public void setUp() throws Exception
     {
+        MockitoAnnotations.initMocks(this);
+
+        contactService = new ContactServiceImpl(contactRepository);
     }
 
     @Test
     public void getContacts()
     {
+        Contact contact=new Contact();
+        contact.setId(1L);
+
+        Set<Contact> contactSet=new HashSet<>();
+        contactSet.add(contact);
+
+        when(contactRepository.findAll()).thenReturn(contactSet);
+
+        assertEquals(1, contactService.getContacts().size());
+        verify(contactRepository,times(1)).findAll();
+
     }
 }
